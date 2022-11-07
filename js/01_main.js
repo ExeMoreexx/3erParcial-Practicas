169eb595caee1c4fedd97273d843d98f1c60b9a1
Nicole Rodriguez <lorettrodriguez1426@gmail.com>
2022-11-07 08:41:34 -0600

    Update

--- js/01_main.js
+++ js/01_main.js
@@ -10,9 +10,11 @@ const cargarPosts = async () => {
         <th scope="row">${item.id}</th>
         <td>${item.title}</td>
         <td>${item.body}</td>
-        <td><button type="button" class="btn btn-primary float-end mb-3"><i class="bi bi-pencil-square"></i>Editar
+        <td><button type="button" class="btn btn-primary float-end mb-3" data-bs-toggle="modal"
+        data-bs-target="#editModal"><i class="bi bi-pencil-square"></i>Editar
       </button></td>
-        <td><button type="button" class="btn btn-danger float-end mb-3"><i class="bi bi-trash3"></i>Eliminar
+        <td><button type="button" class="btn btn-danger float-end mb-3" data-bs-toggle="modal"
+        data-bs-target="#deleteModal"><i class="bi bi-trash3"></i>Eliminar
       </button></td>
     </tr>
   `;
