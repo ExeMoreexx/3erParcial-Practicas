2753e046d4400a06324ec4537c1502165e7c57b6
ExeMoreexx <torreslopezjuandaniel398@gmail.com>
2022-11-09 10:19:38 -0600

    Update

--- js/01_main.js
+++ js/01_main.js
@@ -11,7 +11,7 @@ const cargarPosts = async () => {
         <td>${item.title}</td>
         <td>${item.body}</td>
         <td><button type="button" class="btn btn-primary float-end mb-3" data-bs-toggle="modal"
-        data-bs-target="#editModal"><i class="bi bi-pencil-square"></i>Editar
+        data-bs-target="#editModal"><i class="bi bi-pencil-square" onclick="editPost(${item.id})"></i>Editar
       </button></td>
         <td><button type="button" class="btn btn-danger float-end mb-3" onclick="borrar()"><i class="bi bi-trash3"></i>Eliminar
       </button></td>
@@ -26,41 +26,71 @@ const addPost = async () => {
   const post = {
     title: titulo,
     body: body,
-    userId: 1
+    userId: 1,
   };
+  console.log(post);
 
-  const api = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
     body: JSON.stringify(post),
     headers: {
       "Content-type": "application/json; charset-UTF-8",
     },
   });
 
-  const respuesta = await api.json();
+  const item = await api.json();
+  console.log(item);
   tabla = document.querySelector("#lista");
   tr = document.createElement("tr");
   tr.innerHTML = `
-<th scope="row">${respuesta.id}</th>
-            <td>${respuesta.title}</td>
-            <td>${respuesta.body}</td>
-            <td><button class="btn btn-primary btn-lg"  data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-pencil-square"></i>Editar</button></td>
-            <td><button class="btn btn-danger btn-lg"  data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-trash"></i></i>Eliminar</button></td>
+  <tr>
+  <th scope="row">${item.id}</th>
+        <td>${item.title}</td>
+        <td>${item.body}</td>
+        <td><button type="button" class="btn btn-primary float-end mb-3" data-bs-toggle="modal"
+        data-bs-target="#editModal" onclick="editPost(${item.id})"><i class="bi bi-pencil-square"></i>Editar
+      </button></td>
+        <td><button type="button" class="btn btn-danger float-end mb-3" onclick="borrar(${item.id})"><i class="bi bi-trash3"></i>Eliminar
+      </button></td>
+    </tr>
 `;
   tabla.appendChild(tr);
-  if (respuesta != null) {
+  if (item != null) {
     Swal.fire({
       icon: "success",
-      title: "INSERTAR",
-      text: "SE INSERTO CORRECTAMENTE!!",
+      title: "Insertar",
+      text: "Se Inserto Correctamente!!",
     });
   } else {
     Swal.fire({
       icon: "error",
-      title: "ERROR",
-      text: "ALG0 FALLO!!!",
+      title: "Error!",
+      text: "Algo Fallo!",
     });
   }
+};
+
+const editPost = async (id) => {
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+  const respuesta = await api.json();
+  console.log(respuesta);
+  document.querySelector("#etitle").value = respuesta.title;
+  document.querySelector("#ebody").value = respuesta.body;
+  document.querySelector("#eid").value = respuesta.id;
+  document.querySelector("#euserid").value = respuesta.userId;
+};
+
+const savePost = async () => {
+  document.querySelector("#etitle").value;
+  document.querySelector("#ebody").value;
+  document.querySelector("#eid").value;
+  document.querySelector("#euserid").value;
+  const post = {
+    title: titulo,
+    body: body,
+    id: id,
+    userId: userId,
+  };
 };
 
 const borrar = () => {
