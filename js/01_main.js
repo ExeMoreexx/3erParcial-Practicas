f731b8e0aae1eea25b62940dd356de5932666034
exemoreexx <miguelaguirre7712@gmail.com>
2022-11-10 03:49:29 -0600

    Update

--- js/01_main.js
+++ js/01_main.js
@@ -6,17 +6,75 @@ const cargarPosts = async () => {
   tabla = document.querySelector("#lista");
   data.map((item) => {
     tabla.innerHTML += `
-    <tr>
-        <th scope="row">${item.id}</th>
-        <td>${item.title}</td>
-        <td>${item.body}</td>
-        <td><button type="button" class="btn btn-primary float-end mb-3" data-bs-toggle="modal"
-        data-bs-target="#editModal"><i class="bi bi-pencil-square" onclick="editPost(${item.id})"></i>Editar
-      </button></td>
-        <td><button type="button" class="btn btn-danger float-end mb-3" onclick="borrar()"><i class="bi bi-trash3"></i>Eliminar
-      </button></td>
-    </tr>
-  `;
+      <tr>
+          <th scope="row">${item.id}</th>
+          <td>${item.title}</td>
+          <td>${item.body}</td>
+          <td><button class="btn btn-primary btn-lg"  data-bs-toggle="modal" data-bs-target="#editModal" onclick="cargarPost(${item.id})"><i class="bi bi-pencil-square" ></i>Editar</button></td>
+          <td><button class="btn btn-danger btn-lg" onclick="eliminarPost(${item.id})"><i class="bi bi-trash" ></i></i>Eliminar</button></td>
+      </tr> `;
+  });
+};
+
+const cargarPost = async (id) => {
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+  const respuesta = await api.json();
+  console.log(respuesta);
+  document.querySelector("#etitle").value = respuesta.title;
+  document.querySelector("#ebody").value = respuesta.body;
+  document.querySelector("#eid").value = respuesta.id;
+  document.querySelector("#euserid").value = respuesta.userId;
+};
+
+const guardarPost = async () => {
+  let title = document.querySelector("#etitle").value;
+  let body = document.querySelector("#ebody").value;
+  let id = document.querySelector("#eid").value;
+  let userId = document.querySelector("#euserid").value;
+  const post = {
+    title: title,
+    body: body,
+    id: id,
+    userId: userId,
+  };
+  const api = await fetch("https://jsonplaceholder.typicode.com/posts/" + id, {
+    method: "PUT",
+    body: JSON.stringify(post),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  const respuesta = api.json();
+  console.log(respuesta);
+  if (respuesta != null) {
+    Swal.fire({
+      icon: "success",
+      title: "EDITAR",
+      text: "SE ACTUALIZO CORRECTAMENTE!!",
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "ERROR",
+      text: "ALGO FALLO!!!",
+    });
+  }
+};
+
+const eliminarPost = (id) => {
+  Swal.fire({
+    title: "Delete",
+    text: "Estas seguro!?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, Borrar!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire("Borrado!", "Archivo Exitosamente Borrado!!", "success");
+    }
   });
 };
 
@@ -28,87 +86,39 @@ const addPost = async () => {
     body: body,
     userId: 1,
   };
-  console.log(post);
 
   const api = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     body: JSON.stringify(post),
     headers: {
-      "Content-type": "application/json; charset-UTF-8",
+      "Content-type": "application/json; charset=UTF-8",
     },
   });
 
-  const item = await api.json();
-  console.log(item);
+  const respuesta = await api.json();
   tabla = document.querySelector("#lista");
   tr = document.createElement("tr");
   tr.innerHTML = `
-  <tr>
-  <th scope="row">${item.id}</th>
-        <td>${item.title}</td>
-        <td>${item.body}</td>
-        <td><button type="button" class="btn btn-primary float-end mb-3" data-bs-toggle="modal"
-        data-bs-target="#editModal" onclick="editPost(${item.id})"><i class="bi bi-pencil-square"></i>Editar
-      </button></td>
-        <td><button type="button" class="btn btn-danger float-end mb-3" onclick="borrar(${item.id})"><i class="bi bi-trash3"></i>Eliminar
-      </button></td>
-    </tr>
-`;
+  
+      <th scope="row">${respuesta.id}</th>
+          <td>${respuesta.title}</td>
+          <td>${respuesta.body}</td>
+          <td><button class="btn btn-primary btn-lg"><i class="bi bi-pencil-square"></i>Editar</button></td>
+          <td><button class="btn btn-danger btn-lg" onclick="eliminarPost(${respuesta.id})"><i class="bi bi-trash"></i></i>Eliminar</button></td>
+  
+  `;
   tabla.appendChild(tr);
-  if (item != null) {
+  if (respuesta != null) {
     Swal.fire({
       icon: "success",
-      title: "Insertar",
-      text: "Se Inserto Correctamente!!",
+      title: "INSERTAR",
+      text: "SE INSERTO CORRECTAMENTE!!",
     });
   } else {
     Swal.fire({
       icon: "error",
-      title: "Error!",
-      text: "Algo Fallo!",
+      title: "ERROR",
+      text: "ALG0 FALLO!!!",
     });
   }
-};
-
-const editPost = async (id) => {
-  const api = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
-  const respuesta = await api.json();
-  console.log(respuesta);
-  document.querySelector("#etitle").value = respuesta.title;
-  document.querySelector("#ebody").value = respuesta.body;
-  document.querySelector("#eid").value = respuesta.id;
-  document.querySelector("#euserid").value = respuesta.userId;
-};
-
-const savePost = async () => {
-  document.querySelector("#etitle").value;
-  document.querySelector("#ebody").value;
-  document.querySelector("#eid").value;
-  document.querySelector("#euserid").value;
-  const post = {
-    title: titulo,
-    body: body,
-    id: id,
-    userId: userId,
-  };
-};
-
-const borrar = () => {
-  Swal.fire({
-    title: "Estas Seguro?",
-    text: "No Sera Posible Recuperarlo Despues De Ser Borrado!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Si, Borrar!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        "Exitoso!",
-        "El Archivo Fue Borrado Correctamente!.",
-        "success"
-      );
-    }
-  });
 };
